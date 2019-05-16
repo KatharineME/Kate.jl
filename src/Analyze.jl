0c6a0771ea0bf0e8795b2ffbc0aa22affb36f46e
KwatME <kwatme8@gmail.com>
2019-05-15 21:37:33 -0700

    sync

--- src/Analyze.jl
+++ src/Analyze.jl
@@ -1,9 +1,7 @@
 module Kraft
 
-include("count_transcripts_using_kallisto_quant.jl")
 include("print_command_and_run.jl")
 
-export count_transcripts_using_kallisto_quant
 export print_command_and_run
 
 end
