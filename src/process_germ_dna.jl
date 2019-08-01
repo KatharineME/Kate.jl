1d07a013c3674e68b4db617538461b4f648835cb
KwatME <kwatme8@gmail.com>
2019-08-01 13:28:19 -0700

    2+: process_soma|germ_dna.jl

--- src/process_germ_dna.jl
+++ src/process_germ_dna.jl
@@ -6,24 +6,28 @@ include("find_variant.jl")
 
 
 function process_germ_dna(
-    germ_dna_1_fastq_gz::String,
-    germ_dna_2_fastq_gz::String,
-    dna_is_targeted::Bool,
-    output_dir::String,
-    dna_fasta_gz::String,
-    chromosome_bed_gz::String,
-    chrn_n_tsv::String,
+    germ_dna_1_fastq_gz_file_path::String,
+    germ_dna_2_fastq_gz_file_path::String,
+    # soma_dna_1_fastq_gz_file_path::String,
+    # soma_dna_2_fastq_gz_file_path::String,
+    is_targeted::Bool,
+    output_directory_path::String,
+    dna_fasta_gz_file_path::String,
+    chromosome_bed_gz_file_path::String,
+    chrn_n_tsv_file_path::String,
     n_job::Int,
-    gb_memory::Int,
-    job_gb_memory::Int,
-)
+    n_gb_memory::Int,
+    n_gb_memory_per_job::Int,
+)::Nothing
 
     for file_path in (
-        germ_dna_1_fastq_gz,
-        germ_dna_2_fastq_gz,
-        dna_fasta_gz,
-        chromosome_bed_gz,
-        chrn_n_tsv,
+        germ_dna_1_fastq_gz_file_path,
+        germ_dna_2_fastq_gz_file_path,
+        # soma_dna_1_fastq_gz_file_path,
+        # soma_dna_2_fastq_gz_file_path,
+        dna_fasta_gz_file_path,
+        chromosome_bed_gz_file_path,
+        chrn_n_tsv_file_path,
     )
 
         if !isfile(file_path)
@@ -34,123 +38,122 @@ function process_germ_dna(
 
     end
 
-    if isdir(output_dir)
+    if isdir(output_directory_path)
 
-        error("$output_dir exists.")
+        error("$output_directory_path exists.")
     
-    else
+    mkpath(output_directory_path)
 
-        mkpath(output_dir)
-    
-    end
-
-    germ_trim_sequence_prefix = joinpath(
-        output_dir,
+    germ_trim_sequence_file_path_prefix = joinpath(
+        output_directory_path,
         "trim_sequence",
         "germ",
     )
 
     trim_sequence(
-        germ_dna_1_fastq_gz,
-        germ_dna_2_fastq_gz,
-        germ_trim_sequence_prefix,
+        germ_dna_1_fastq_gz_file_path,
+        germ_dna_2_fastq_gz_file_path,
+        germ_trim_sequence_file_path_prefix,
         n_job,
     )
 
-    germ_trim_1_fastq_gz = "$germ_trim_sequence_prefix-trimmed-pair1.fastq.gz"
+    germ_trim_1_fastq_gz_file_path = "$germ_trim_sequence_file_path_prefix-trimmed-pair1.fastq.gz"
 
-    germ_trim_2_fastq_gz = "$germ_trim_sequence_prefix-trimmed-pair2.fastq.gz"
+    germ_trim_2_fastq_gz_file_path = "$germ_trim_sequence_file_path_prefix-trimmed-pair2.fastq.gz"
 
-    # soma_trim_sequence_prefix = joinpath(
-    #     output_dir,
+    # soma_trim_sequence_file_path_prefix = joinpath(
+    #     output_directory_path,
     #     "trim_sequence",
     #     "soma",
     # )
 
     # trim_sequence(
-    #     soma_dna_1_fastq_gz,
-    #     soma_dna_2_fastq_gz,
-    #     soma_trim_sequence_prefix,
+    #     soma_dna_1_fastq_gz_file_path,
+    #     soma_dna_2_fastq_gz_file_path,
+    #     soma_trim_sequence_file_path_prefix,
     #     n_job,
     # )
 
-    # soma_trim_1_fastq_gz = "$soma_trim_sequence_prefix-trimmed-pair1.fastq.gz"
+    # soma_trim_1_fastq_gz_file_path = "$soma_trim_sequence_file_path_prefix-trimmed-pair1.fastq.gz"
 
-    # soma_trim_2_fastq_gz = "$soma_trim_sequence_prefix-trimmed-pair2.fastq.gz"
+    # soma_trim_2_fastq_gz_file_path = "$soma_trim_sequence_file_path_prefix-trimmed-pair2.fastq.gz"
 
     check_sequence(
         (
-         germ_trim_1_fastq_gz,
-         germ_trim_2_fastq_gz,
+         germ_trim_1_fastq_gz_file_path,
+         germ_trim_2_fastq_gz_file_path,
+        #  soma_trim_1_fastq_gz_file_path,
+        #  soma_trim_2_fastq_gz_file_path,
         ),
         joinpath(
-            output_dir,
+            output_directory_path,
             "check_sequence",
         ),
         n_job,
     )
 
-    germ_bam = joinpath(
-        output_dir,
+    germ_bam_file_path = joinpath(
+        output_directory_path,
         "align_sequence",
         "germ.bam",
     )
 
     align_sequence(
-        germ_trim_1_fastq_gz,
-        germ_trim_2_fastq_gz,
+        germ_trim_1_fastq_gz_file_path,
+        germ_trim_2_fastq_gz_file_path,
         "Germ",
-        dna_fasta_gz,
-        germ_bam,
+        dna_fasta_gz_file_path,
+        germ_bam_file_path,
         n_job,
-        job_gb_memory,
+        n_gb_memory_per_job,
     )
 
-    # soma_bam = joinpath(
-    #     output_dir,
+    # soma_bam_file_path = joinpath(
+    #     output_directory_path,
     #     "align_sequence",
     #     "soma.bam",
     # )
 
     # align_sequence(
-    #     soma_trim_1_fastq_gz,
-    #     soma_trim_2_fastq_gz,
+    #     soma_trim_1_fastq_gz_file_path,
+    #     soma_trim_2_fastq_gz_file_path,
     #     "Soma",
-    #     dna_fasta_gz,
-    #     soma_bam,
+    #     dna_fasta_gz_file_path,
+    #     soma_bam_file_path,
     #     n_job,
-    #     job_gb_memory,
+    #     n_gb_memory_per_job,
     # )
 
-    dna_fasta = splitext(dna_fasta_gz)[1]
+    dna_fasta_bgz_file_path = String(
+        splitext(dna_fasta_gz_file_path)[1],
+        ".bgz",
+    )
 
-    dna_fasta_bgz = "$dna_fasta.bgz"
-
-    if !isfile(dna_fasta_bgz)
+    if !isfile(dna_fasta_bgz_file_path)
 
         print_and_run_cmd(pipeline(
-            `gzip --decompress $dna_fasta_gz --stdout`,
+            `gzip --decompress $dna_fasta_gz_file_path --stdout`,
             `bgzip --threads $n_job --stdout`,
-            dna_fasta_bgz,
+            dna_fasta_bgz_file_path,
         ))
 
     end
 
-    find_variant_dir = joinpath(
-        output_dir,
+    find_variant_directory_path = joinpath(
+        output_directory_path,
         "find_variant",
     )
 
     find_variant(
-        germ_bam,
-        nothing,
-        dna_is_targeted,
-        dna_fasta_bgz,
-        chromosome_bed_gz,
-        chrn_n_tsv,
-        find_variant_dir,
+        germ_bam_file_path,
+        nothing,# soma_bam_file_path,
+        is_targeted,
+        dna_fasta_bgz_file_path,
+        chromosome_bed_gz_file_path,
+        chrn_n_tsv_file_path,
+        find_variant_directory_path,
         n_job,
-        gb_memory,
+        n_gb_memory,
     )
 
 end
--- src/process_soma_rna.jl
+++ src/process_soma_rna.jl
@@ -4,17 +4,17 @@ include("count_transcript.jl")
 
 
 function process_soma_rna(
-    soma_rna_1_fastq_gz::String,
-    soma_rna_2_fastq_gz::String,
-    output_dir::String,
-    cdna_fasta_gz::String,
+    soma_rna_1_fastq_gz_file_path::String,
+    soma_rna_2_fastq_gz_file_path::String,
+    output_directory_path::String,
+    cdna_fasta_gz_file_path::String,
     n_job::Int,
-)
+)::Nothing
 
     for file_path in (
-        soma_rna_1_fastq_gz,
-        soma_rna_2_fastq_gz,
-        cdna_fasta_gz,
+        soma_rna_1_fastq_gz_file_path,
+        soma_rna_2_fastq_gz_file_path,
+        cdna_fasta_gz_file_path,
     )
 
         if !isfile(file_path)
@@ -25,55 +25,51 @@ function process_soma_rna(
 
     end
 
-    if isdir(output_dir)
+    if isdir(output_directory_path)
 
-        error("$output_dir exists.")
+        error("$output_directory_path exists.")
     
-    else
+    mkpath(output_directory_path)
 
-        mkpath(output_dir)
-    
-    end
-
-    soma_trim_sequence_prefix = joinpath(
-        output_dir,
+    soma_trim_sequence_file_path_prefix = joinpath(
+        output_directory_path,
         "trim_sequence",
         "soma",
     )
 
     trim_sequence(
-        soma_rna_1_fastq_gz,
-        soma_rna_2_fastq_gz,
-        soma_trim_sequence_prefix,
+        soma_rna_1_fastq_gz_file_path,
+        soma_rna_2_fastq_gz_file_path,
+        soma_trim_sequence_file_path_prefix,
         n_job,
     )
 
-    soma_trim_1_fastq_gz = "$soma_trim_sequence_prefix-trimmed-pair1.fastq.gz"
+    soma_trim_1_fastq_gz_file_path = "$soma_trim_sequence_file_path_prefix-trimmed-pair1.fastq.gz"
 
-    soma_trim_2_fastq_gz = "$soma_trim_sequence_prefix-trimmed-pair2.fastq.gz"
+    soma_trim_2_fastq_gz_file_path = "$soma_trim_sequence_file_path_prefix-trimmed-pair2.fastq.gz"
 
     check_sequence(
         (
-         soma_trim_1_fastq_gz,
-         soma_trim_2_fastq_gz,
+         soma_trim_1_fastq_gz_file_path,
+         soma_trim_2_fastq_gz_file_path,
         ),
         joinpath(
-            output_dir,
+            output_directory_path,
             "check_sequence",
         ),
         n_job,
     )
 
-    count_transcript_dir = joinpath(
-        output_dir,
+    count_transcript_directory_path = joinpath(
+        output_directory_path,
         "count_transcript",
     )
 
     count_transcript(
-        soma_trim_1_fastq_gz,
-        soma_trim_2_fastq_gz,
-        cdna_fasta_gz,
-        count_transcript_dir,
+        soma_trim_1_fastq_gz_file_path,
+        soma_trim_2_fastq_gz_file_path,
+        cdna_fasta_gz_file_path,
+        count_transcript_directory_path,
         n_job,
     )
 
