71dc1b974de9a6b10d7c9500e7a78e78133aeaf2
KatharineME <katharine.me@icloud.com>
2020-12-07 23:54:20 -0800

    find_reads

--- src/find_read.jl
+++ src/find_read.jl
@@ -1,4 +1,49 @@
 using Dates
 
-include("print_and_run_cmd.jl")
+
+function find_reads(sample_dir::String)
+    
+    start_time = now()
+    
+    number_of_fastq_files = 0
+    number_of_fastq_gz_files = 0
+    fastq_files_to_check = []
+    
+    println("Walking sample directory...\n")
+
+    for (root, dirs, files) in walkdir("$sample_dir")
+        
+        println("$root\n")
+        
+        for file in files
+            if occursin(".fastq", file)
+                number_of_fastq_files += 1
+            end
+            if occursin("fastq.gz", file)
+                number_of_fastq_gz_files += 1
+                push!(fastq_files_to_check, joinpath(root, file))
+            end
+            if occursin("fq.gz", file)
+                number_of_fastq_gz_files += 1
+                push!(fastq_files_to_check, joinpath(root, file))
+            end
+        end
+    end
+
+    println("\nNumber of fastq files found in directories walked: $number_of_fastq_files\n")
+
+    println("Number of fastq.gz or fq.gz files found in directories walked: $number_of_fastq_gz_files\n")
+
+    println(string("Number of fastq.gz or fq.gz files to be checked: ", length(fastq_files_to_check)))
+    
+    end_time = now()
+    
+    println("\nDone at: $end_time\n")
+    
+    println("Took $(canonicalize(Dates.CompoundPeriod(end_time - start_time))).\n")
+    
+    return fastq_files_to_check
+    
+end
+
 
