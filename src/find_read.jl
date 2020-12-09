39e60c1939351367f3fc08a02f78f1ddd22f08e1
KatharineME <katharine.me@icloud.com>
2020-12-08 23:13:06 -0800

    find_read

--- src/find_read.jl
+++ src/find_read.jl
@@ -6,7 +6,9 @@ function find_reads(sample_dir::String)
     start_time = now()
     
     number_of_fastq_files = 0
+
     number_of_fastq_gz_files = 0
+
     fastq_files_to_check = []
     
     println("Walking sample directory...\n")
@@ -16,18 +18,30 @@ function find_reads(sample_dir::String)
         println("$root\n")
         
         for file in files
+
             if occursin(".fastq", file)
+
                 number_of_fastq_files += 1
             end
+
             if occursin("fastq.gz", file)
-                number_of_fastq_gz_files += 1
+                
+		number_of_fastq_gz_files += 1
+
                 push!(fastq_files_to_check, joinpath(root, file))
+
             end
+
             if occursin("fq.gz", file)
+
                 number_of_fastq_gz_files += 1
+
                 push!(fastq_files_to_check, joinpath(root, file))
+
             end
+
         end
+
     end
 
     println("\nNumber of fastq files found in directories walked: $number_of_fastq_files\n")
@@ -45,5 +59,3 @@ function find_reads(sample_dir::String)
     return fastq_files_to_check
     
 end
-
-
